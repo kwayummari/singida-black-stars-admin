149048625bd81caf091a8eb4f58ea5a8040897aa
Kwayu Mmari <mmaridenis@gmail.com>
2024-10-06 22:13:00 +0300

    deleted page module.css

--- connection/index.php
+++ connection/index.php
@@ -2,7 +2,7 @@
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
-$connect=$db =mysqli_connect('localhost','u750269652_pandadigital','PandaDigital.2020','u750269652_pandadigital')
+$connect=$db =mysqli_connect('localhost','root','','singidaBlackStars')
 or die("connection Failed"); 
 
 
--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include "../../admin/includes/db/connect.php";
+include "./connection/connection2.php";
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $email = trim($_POST['email']);
